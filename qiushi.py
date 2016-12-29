7960b6ea8510977ab702508eb66fc7eacea85478
[zhiming] <[915840827@qq.com]>
2016-12-29 18:09:05 +0800

    top250

--- qiushi.py
+++ qiushi.py
@@ -1,48 +1,40 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Mar 04 10:48:10 2016
-@author: guohuaiqi
-"""
-import numpy as np
-import pandas as pd
-from pandas import Series,DataFrame
-from bs4 import BeautifulSoup
+# -*-encoding:utf-8 -*-
 import requests
-import time
+import xlwt
+from bs4 import BeautifulSoup
 
-urls=['http://www.qiushibaike.com/hot/page/{}/?s=4942752'.format(str(i)) for i in range(1,3,1)]
-#url='https://movie.douban.com/mine?status=wish'
-headers={
-    'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36',
-    'Cookie':'_xsrf=2|de05d702|4bd90624827119dd0b4ffadd44e59667|1482825706; _qqq_uuid_="2|1:0|10:1482825706|10:_qqq_uuid_|56:YjcyYzI2MmM0MjY4Y2I1MjVhMGQxMzgwNTRhOTM0NjA2NzA3ZDJjYQ==|f10a5dc96c6bb5eb4e8f2141c616a57aa64be889aae4440e031b8038aa145289"; afpCT=1; _gat=1; Hm_lvt_2670efbdd59c7e3ed3749b458cafaa37=1481371732,1481373557,1481437471,1482825708; Hm_lpvt_2670efbdd59c7e3ed3749b458cafaa37=1482825731; _ga=GA1.2.323694707.1482825711'
-    }
 
-title=[]    
-support = []
-content = []
-#爬取并显示top250电影的名称，图片地址，演员等
-def allfilm(web_url,data=None):
-    web_data=requests.get(web_url)
-    soup=BeautifulSoup(web_data.text,'lxml')
-    time.sleep(2)
-    titles = soup.select('#content > div > div.col1 > div > div.author > a')
-    #supports = soup.select('#content > div > div >div > div.status >span.stats-vote > i.number')
-    contents = soup.select('#content > div > div.col1 > div > a > div > span')
-    print (titles)
-    print(contents)
-    for title1,content1  in zip(titles,contents):
-        title.append(title1.get_text())
-        #support.append(support1)
-        content.append(content1.get_text())
 
-for sigle_url in urls:
-    allfilm(sigle_url)
-data={'标题':title,
-      '内容':content,
-      }
-frame=DataFrame(data,columns=[u'标题',u'内容'])
-print('done!!')
+def get_data(url):
+    res = requests.get(url)
+    soup = BeautifulSoup(res.text,'lxml')
 
-#将dataframe数据写入csv或xlsx文件
-frame.to_csv('C:/Users/Administrator/Desktop/scrapy/2.csv', decode='gb2312', index=True)
 
+    cont = soup.select('.content')
+    auth = soup.select('.author h2')
+    for con,aut in zip(cont,auth):
+        if con and aut:
+            content.append(con.text.split('\n'))
+            author.append(aut.text)
+    return author,content
+    
+def write_excel(book,sheet_name,author,content):
+     
+    sheet = book.add_sheet(sheet_name)
+    sheet.write(0,0,'作者')
+    sheet.write(0,1,'内容')
+    for i in range(len(author)):
+        sheet.write(i+1,0,author[i])
+        sheet.write(i+1,1,content[i])
+    book.save('./qiushi.xls')
+    
+if __name__ == "__main__":
+    book = xlwt.Workbook(encoding='utf-8')
+    web_url = ["http://www.qiushibaike.com/8hr/page/{}/?s=4943272" .format(str(i)) for i in range (1,5,1)]
+    sheet_name = '糗事'
+    author = list()
+    content = list()
+    for url in web_url:
+        print (url)
+        get_data(url)
+    write_excel(book,sheet_name,author,content)
